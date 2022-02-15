4b6211ea0fe38585816651cea8e7ddd5b75c51d8
Giovanna Cinoca <72810976+ginocax@users.noreply.github.com>
2022-02-15 19:04:26 -0300

    backup

--- scriptFeed.js
+++ scriptFeed.js
@@ -38,3 +38,39 @@ estiloBarra.addEventListener('mouseout', function(){
 // 4
 document.getElementById("more").style.cursor = "pointer";
 }
+
+// validação
+
+let form = document.querySelector('.form-auth');
+
+window.onload = function() {
+    let formUser = document.querySelector('.form-auth input[name="username"]');
+    let inputData = document.createElement('input')
+    inputData.setAttribute('type', 'date')
+    inputData.setAttribute('name', 'dataNascimento')
+    form.insertBefore(inputData, formUser);
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let erro = 0;
+    let inputs = document.querySelectorAll('.form-auth input');
+    let mensagemErro = document.querySelector('.erro');
+    mensagemErro.style.color - '#red';
+
+    for (const input of inputs){
+        if (input.value == "") {
+            erro++;
+        }
+    };
+
+    if (erro > 0) {
+            mensagemErro.innerText = 'Ops campo vazio';
+            
+        } else {
+            form.submit();
+        }
+
+});
+
+
